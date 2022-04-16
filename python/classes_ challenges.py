3fb6af36f41eca1a85638262d873409dc71d7638
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-16 16:46:37 +0200

    count x letter in any word

--- python/classes_ challenges.py
+++ python/classes_ challenges.py
@@ -1,4 +1,5 @@
 # 1. Count Letters
+print("1. Count Letters =====================================================")
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 # Write your unique_english_letters function here:
 def unique_english_letters(word):
@@ -16,3 +17,24 @@ print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+# #########################################
+# 2. Count X
+print("2. Count X =====================================================")
+# Write your count_char_x function here:
+def count_char_x(word, x):
+    letters = {}
+
+    for letter in word:
+        if letter not in letters.keys():
+            letters[letter] = 1
+        else:
+            letters[letter] += 1
+    return letters[x]
+
+
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
