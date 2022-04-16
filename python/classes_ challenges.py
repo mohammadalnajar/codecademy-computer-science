d464a686945562870519554c9b8c5d1bd7cd23bf
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-16 17:28:20 +0200

    x_length_words func

--- python/classes_ challenges.py
+++ python/classes_ challenges.py
@@ -77,3 +77,23 @@ print(substring_between_letters("apple", "p", "e"))
 print(substring_between_letters("apple", "c", "c"))
 
 # should print "apple"
+
+# 5. X Length
+print("5. X Length =====================================================")
+
+# Write your x_length_words function here:
+def x_length_words(sentence, num):
+    words = sentence.split()
+    print(words)
+    bool = True
+    for word in words:
+        if len(word) < num:
+            bool = False
+    return bool
+
+
+# Uncomment these function calls to test your tip function:
+print(x_length_words("i like apples", 2))
+# should print False
+print(x_length_words("he likes apples", 2))
+# should print True
