21acafa6296f495bab694c3c96fd4d86068de2df
Mohammad-Al-Najar <44784702+mohammadalnajar@users.noreply.github.com>
2022-04-17 03:18:30 +0200

    add advanced challenges dictionaries

--- python/dictionaries_challenges.py
+++ python/dictionaries_challenges.py
@@ -89,3 +89,106 @@ print(max_key({1: 100, 2: 1, 3: 4, 4: 10}))
 # should print 1
 print(max_key({"a": 100, "b": 10, "c": 1000}))
 # should print "c"
+
+print("==============================ADVANCED==============================")
+
+# 1. Word Length Dict
+print("1. Word Length Dict =====================================================")
+
+# Write your word_length_dictionary function here:
+def word_length_dictionary(list_of_words):
+    dict = {}
+    for word in list_of_words:
+        dict[word] = len(word)
+
+    return dict
+
+
+# Uncomment these function calls to test your  function:
+print(word_length_dictionary(["apple", "dog", "cat"]))
+# should print {"apple":5, "dog": 3, "cat":3}
+print(word_length_dictionary(["a", ""]))
+# should print {"a": 1, "": 0}
+
+# 2. Frequency Count
+print("2. Frequency Count =====================================================")
+
+# Write your frequency_dictionary function here:
+def frequency_dictionary(list_of_values):
+    dict = {}
+    for val in list_of_values:
+        dict[val] = list_of_values.count(val)
+
+    return dict
+
+
+# Uncomment these function calls to test your  function:
+print(frequency_dictionary(["apple", "apple", "cat", 1]))
+# should print {"apple":2, "cat":1, 1:1}
+print(frequency_dictionary([0, 0, 0, 0, 0]))
+# should print {0:5}
+
+# 3. Unique Values
+print("3. Unique Values =====================================================")
+
+# Write your unique_values function here:
+
+
+def unique_values(dict):
+    values = []
+    for val in dict.values():
+        values.append(val)
+    print(values, "values")
+    for val in dict.values():
+        print(values.count(val), "val")
+        if values.count(val) > 1:
+            values.remove(val)
+
+    # for key in dict:
+    #     if
+
+    print(values)
+
+
+# Uncomment these function calls to test your  function:
+print(unique_values({0: 3, 1: 1, 4: 1, 5: 3}))
+# should print 2
+print(unique_values({0: 3, 1: 3, 4: 3, 5: 3}))
+# should print 1
+
+# 4. Count First Letter
+print("4. Count First Letter =====================================================")
+
+# Write your count_first_letter function here:
+def count_first_letter(dict):
+
+    dict_to_return = {}
+    for key in dict:
+        if not key[0] in dict_to_return:
+            dict_to_return[key[0]] = len(dict[key])
+        else:
+            dict_to_return[key[0]] += len(dict[key])
+    return dict_to_return
+
+
+# Uncomment these function calls to test your  function:
+print(
+    count_first_letter(
+        {
+            "Stark": ["Ned", "Robb", "Sansa"],
+            "Snow": ["Jon"],
+            "Lannister": ["Jaime", "Cersei", "Tywin"],
+        }
+    )
+)
+# should print {"S": 4, "L": 3}
+print(
+    count_first_letter(
+        {
+            "Stark": ["Ned", "Robb", "Sansa"],
+            "Snow": ["Jon"],
+            "Sannister": ["Jaime", "Cersei", "Tywin"],
+        }
+    )
+)
+# should print {"S": 7}
